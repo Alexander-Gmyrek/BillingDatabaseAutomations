99f2bf7350484baeced175968c0a14724c356ca7
Alexander-Gmyrek <agdaboss12@gmail.com>
2024-08-21 12:43:07 -0700

    maybe now work?

--- setup.bat
+++ setup.bat
@@ -1,54 +1,54 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Read from the config file
+REM Read from the config file and store each setting as a variable
 for /f "tokens=1,2 delims== " %%a in ('type config.ini ^| findstr /r "^[^;]"') do (
     set "%%a=%%b"
 )
 
-REM Setting up backup.bat (improved version)
-set "file=backup.bat"
+REM Replace placeholders in backup.bat
+set "input=backup.bat"
 set "output=backup_final.bat"
 
 echo. > "%output%"
-for /f "delims=" %%a in (%file%) do (
+for /f "delims=" %%a in (%input%) do (
     set "line=%%a"
     set "line=!line:MYSQL_USER=%MySQL_user%!"
     set "line=!line:MYSQL_PASSWORD=%MySQL_password%!"
     set "line=!line:MYSQL_DATABASE=%MySQL_database%!"
     set "line=!line:BACKUP_DIR=%Paths_backup_dir%!"
-    set "line=!line:BACKUP_FILE=%Paths_backup_dir%\%Backup_backup_file_format%!"
+    set "line=!line:BACKUP_FILE=%Backup_backup_file_format%!"
     echo !line! >> "%output%"
 )
 
-REM Setting up deploy.bat (improved version)
-set "file=deploy.bat"
+REM Replace placeholders in deploy.bat
+set "input=deploy.bat"
 set "output=deploy_final.bat"
 
 echo. > "%output%"
-for /f "delims=" %%a in (%file%) do (
+for /f "delims=" %%a in (%input%) do (
     set "line=%%a"
     set "line=!line:C:\\path\\to\\your\\docker-compose\\files=%Paths_docker_compose_dir%!"
     echo !line! >> "%output%"
 )
 
-REM Setting up restart.bat (improved version)
-set "file=restart.bat"
+REM Replace placeholders in restart.bat
+set "input=restart.bat"
 set "output=restart_final.bat"
 
 echo. > "%output%"
-for /f "delims=" %%a in (%file%) do (
+for /f "delims=" %%a in (%input%) do (
     set "line=%%a"
     set "line=!line:C:\\path\\to\\your\\docker-compose\\files=%Paths_docker_compose_dir%!"
     echo !line! >> "%output%"
 )
 
-REM Setting up restore.bat (improved version)
-set "file=restore.bat"
+REM Replace placeholders in restore.bat
+set "input=restore.bat"
 set "output=restore_final.bat"
 
 echo. > "%output%"
-for /f "delims=" %%a in (%file%) do (
+for /f "delims=" %%a in (%input%) do (
     set "line=%%a"
     set "line=!line:MYSQL_USER=%MySQL_user%!"
     set "line=!line:MYSQL_PASSWORD=%MySQL_password%!"
@@ -58,12 +58,12 @@ for /f "delims=" %%a in (%file%) do (
     echo !line! >> "%output%"
 )
 
-REM Setting up update.bat (improved version)
-set "file=update.bat"
+REM Replace placeholders in update.bat
+set "input=update.bat"
 set "output=update_final.bat"
 
 echo. > "%output%"
-for /f "delims=" %%a in (%file%) do (
+for /f "delims=" %%a in (%input%) do (
     set "line=%%a"
     set "line=!line:repoPath=%Paths_repo_path%!"
     set "line=!line:lockFile=%Paths_repo_path%\\deploy.lock!"
